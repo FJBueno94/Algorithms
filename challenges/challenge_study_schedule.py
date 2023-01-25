4ae1f0f75d1de0661a502a4f098169499b4bd5e9
FJBueno94 <fernando_jordi@hotmail.com>
2023-01-25 16:18:43 -0300

    implements function study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+
+    for start, end in permanence_period:
+        if (start not in range(0, 24)) or (end not in range(0, 24)):
+            return None
+
+        if not target_time:
+            return None
+
+        if start <= target_time <= end:
+            counter += 1
+
+    return counter

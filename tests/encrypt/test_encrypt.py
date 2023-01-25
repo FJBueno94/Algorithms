1187804270453becb940dc53f2f463415dbd7f8b
FJBueno94 <fernando_jordi@hotmail.com>
2023-01-25 18:29:35 -0300

    make the test to function encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,26 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    invalid_key = "Hello"
+    invalid_message = 2
+
+    with pytest.raises(TypeError):
+        encrypt_message(invalid_message, invalid_key)
+
+# m no final da mensagem devido a palavra "Hello"
+# ordenada de forma decrescente ser igual ao resultado esperado
+    valid_message = "Hellom"
+    valid_key_even = 2
+
+    assert encrypt_message(valid_message, valid_key_even) == "moll_eH"
+
+    valid_key_odd = 3
+
+    assert encrypt_message(valid_message, valid_key_odd) == "leH_mol"
+
+    bigger_key = 10
+
+    assert encrypt_message(valid_message, bigger_key) == "molleH"
+

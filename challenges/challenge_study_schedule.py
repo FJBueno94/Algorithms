d7e10b8689acc8c1105a603e097629178db83a8f
FJBueno94 <fernando_jordi@hotmail.com>
2023-01-25 16:37:05 -0300

    fix study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,7 +2,7 @@ def study_schedule(permanence_period, target_time):
     counter = 0
 
     for start, end in permanence_period:
-        if (start not in range(0, 24)) or (end not in range(0, 24)):
+        if (type(start) is not int) or (type(end) is not int):
             return None
 
         if not target_time:
